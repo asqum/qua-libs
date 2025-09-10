aa807af25addc6d73ee3a8940a9f714be6ffa6c1
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-09-10 18:00:13 +0800

    Update calibrated state json

---
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/RatisRetuneV1.py
@@ -0,0 +1,130 @@
+from typing import List
+from qualibrate.orchestration.basic_orchestrator import BasicOrchestrator
+from qualibrate.parameters import GraphParameters
+from qualibrate.qualibration_graph import QualibrationGraph
+from qualibrate.qualibration_library import QualibrationLibrary
+from time import time
+start = time()
+library = QualibrationLibrary.get_active_library()
+
+
+class Parameters(GraphParameters):
+    qubits: List[str] = None
+
+
+qubits = ["q1", "q2", "q3", "q4", "q5"]
+multiplexed = True
+reset_type_thermal_or_active = "active"
+SQRB_max_gate_num = 900
+pts_fit_RB = 45
+Rabi_state_avg = 80
+
+nodes = [
+    "close_other_qms",
+    "IQ_blobs_before",
+    "ramsey_flux_calibration_1",
+    "ramsey_flux_calibration_2",
+    "ramsey_flux_calibration_3",
+    "power_rabi_x180_before",
+    "power_rabi_x90_before",
+    "DRAG_alpha",
+    "power_rabi_x180_after",
+    "power_rabi_x90_after",
+    "IQ_blobs_after",
+    "SQ_RB",
+]
+
+g = QualibrationGraph(
+    name="Retuning_Graph",
+    parameters=Parameters(qubits=qubits),
+    nodes={
+        "close_other_qms": library.nodes["00_Close_other_QMs"].copy(
+            name="close_other_qms",
+        ),
+        "IQ_blobs_before": library.nodes["07b_IQ_Blobs"].copy(
+            flux_point_joint_or_independent="independent",
+            multiplexed=multiplexed,
+            name="IQ_blobs_before",
+            reset_type_thermal_or_active="thermal",
+        ),
+        "ramsey_flux_calibration_1": library.nodes["06a_Ramsey_vs_Flux_Calibration"].copy(
+            flux_point_joint_or_independent="independent", multiplexed=multiplexed, num_averages=120
+        ),
+        "ramsey_flux_calibration_2": library.nodes["06a_Ramsey_vs_Flux_Calibration"].copy(
+            flux_point_joint_or_independent="independent", multiplexed=multiplexed, num_averages=120
+        ),
+        "ramsey_flux_calibration_3": library.nodes["06a_Ramsey_vs_Flux_Calibration"].copy(
+            flux_point_joint_or_independent="independent", multiplexed=multiplexed, num_averages=120
+        ),
+        "power_rabi_x180_before": library.nodes["09_Power_Rabi_State"].copy(
+            num_averages=Rabi_state_avg,
+            flux_point_joint_or_independent="independent",
+            operation_x180_or_any_90="x180",
+            reset_type_thermal_or_active=reset_type_thermal_or_active,
+            min_amp_factor=0.95,
+            max_amp_factor=1.05,
+            amp_factor_step=0.002,
+            max_number_rabi_pulses_per_sweep=200
+        ),
+        "power_rabi_x90_before": library.nodes["09_Power_Rabi_State"].copy(
+            num_averages=int(1.2*Rabi_state_avg),
+            flux_point_joint_or_independent="independent",
+            operation_x180_or_any_90="x90",
+            reset_type_thermal_or_active=reset_type_thermal_or_active,
+            min_amp_factor=0.9,
+            max_amp_factor=1.1,
+            amp_factor_step=0.004,
+            max_number_rabi_pulses_per_sweep=88
+        ),
+        "DRAG_alpha": library.nodes["09c_DRAG_Calibration_180_90"].copy(
+            flux_point_joint_or_independent="independent", multiplexed=multiplexed,reset_type_thermal_or_active=reset_type_thermal_or_active,
+        ),
+        "IQ_blobs": library.nodes["07b_IQ_Blobs"].copy(
+            flux_point_joint_or_independent="independent",
+            multiplexed=multiplexed,
+            name="IQ_blobs",
+            reset_type_thermal_or_active="thermal",
+        ),
+        "power_rabi_x180_after": library.nodes["09_Power_Rabi_State"].copy(
+            num_averages=Rabi_state_avg,
+            flux_point_joint_or_independent="independent",
+            operation_x180_or_any_90="x180",
+            reset_type_thermal_or_active=reset_type_thermal_or_active,
+            min_amp_factor=0.95,
+            max_amp_factor=1.05,
+            amp_factor_step=0.002,
+            max_number_rabi_pulses_per_sweep=200
+        ),
+        "power_rabi_x90_after": library.nodes["09_Power_Rabi_State"].copy(
+            num_averages=int(1*Rabi_state_avg),
+            flux_point_joint_or_independent="independent",
+            operation_x180_or_any_90="x90",
+            reset_type_thermal_or_active=reset_type_thermal_or_active,
+            min_amp_factor=0.9,
+            max_amp_factor=1.1,
+            amp_factor_step=0.004,
+            max_number_rabi_pulses_per_sweep=88
+        ),
+        "IQ_blobs_after": library.nodes["07b_IQ_Blobs"].copy(
+            flux_point_joint_or_independent="independent",
+            multiplexed=multiplexed,
+            name="IQ_blobs_after",
+            reset_type_thermal_or_active="thermal",
+        ),
+        "SQ_RB": library.nodes["10a_Single_Qubit_Randomized_Benchmarking"].copy(
+            flux_point_joint_or_independent="independent",
+            multiplexed=multiplexed, 
+            delta_clifford=int(SQRB_max_gate_num/pts_fit_RB),
+            max_circuit_depth=SQRB_max_gate_num,
+            reset_type_thermal_or_active=reset_type_thermal_or_active,
+            num_random_sequences=73,
+            num_averages=37
+        ),
+    },
+    connectivity=[(a, b) for a, b in zip(nodes, nodes[1:])],
+    orchestrator=BasicOrchestrator(skip_failed=True),
+)
+
+g.run(qubits=qubits)
+end = time()
+print(f"\n Time consuming: {int(end-start)} sec")
