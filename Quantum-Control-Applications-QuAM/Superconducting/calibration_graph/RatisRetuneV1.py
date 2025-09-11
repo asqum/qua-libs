3056268685e18bcda34232985138b8e71dda2efa
RatisWu <61738702+RatisWu@users.noreply.github.com>
2025-09-11 21:45:50 +0800

    update 5q4c state and add routine calibration graph

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/RatisRetuneV1.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/RatisRetuneV1.py
@@ -25,6 +25,8 @@ nodes = [
     "ramsey_flux_calibration_1",
     "ramsey_flux_calibration_2",
     "ramsey_flux_calibration_3",
+    # "RO_freq",
+    "RO_power",
     "power_rabi_x180_before",
     "power_rabi_x90_before",
     "DRAG_alpha",
@@ -35,7 +37,7 @@ nodes = [
 ]
 
 g = QualibrationGraph(
-    name="Retuning_Graph",
+    name="Ratis_Retuning_Graph",
     parameters=Parameters(qubits=qubits),
     nodes={
         "close_other_qms": library.nodes["00_Close_other_QMs"].copy(
@@ -56,24 +58,37 @@ g = QualibrationGraph(
         "ramsey_flux_calibration_3": library.nodes["06a_Ramsey_vs_Flux_Calibration"].copy(
             flux_point_joint_or_independent="independent", multiplexed=multiplexed, num_averages=120
         ),
+        # "RO_freq": library.nodes["07a_Readout_Frequency_Optimization"].copy(
+        #     flux_point_joint_or_independent="independent", multiplexed=multiplexed,
+        #     num_averages=120 ,
+        #     frequency_span_in_mhz=25,
+        #     frequency_step_in_mhz=0.25
+        # ),
+        "RO_power": library.nodes["07c_Readout_Power_Optimization"].copy(
+            flux_point_joint_or_independent="independent", multiplexed=multiplexed,reset_type_thermal_or_active="thermal",
+            num_runs=2000,
+            start_amp=0.8,
+            end_amp=1.2,
+            num_amps=50
+        ),
         "power_rabi_x180_before": library.nodes["09_Power_Rabi_State"].copy(
             num_averages=Rabi_state_avg,
             flux_point_joint_or_independent="independent",
             operation_x180_or_any_90="x180",
             reset_type_thermal_or_active=reset_type_thermal_or_active,
-            min_amp_factor=0.95,
-            max_amp_factor=1.05,
-            amp_factor_step=0.002,
-            max_number_rabi_pulses_per_sweep=200
+            min_amp_factor=0.8,
+            max_amp_factor=1.2,
+            amp_factor_step=0.008,
+            max_number_rabi_pulses_per_sweep=88
         ),
         "power_rabi_x90_before": library.nodes["09_Power_Rabi_State"].copy(
             num_averages=int(1.2*Rabi_state_avg),
             flux_point_joint_or_independent="independent",
             operation_x180_or_any_90="x90",
             reset_type_thermal_or_active=reset_type_thermal_or_active,
-            min_amp_factor=0.9,
-            max_amp_factor=1.1,
-            amp_factor_step=0.004,
+            min_amp_factor=0.8,
+            max_amp_factor=1.2,
+            amp_factor_step=0.008,
             max_number_rabi_pulses_per_sweep=88
         ),
         "DRAG_alpha": library.nodes["09c_DRAG_Calibration_180_90"].copy(
