b5f7fbe5041155c959690d0f51eb391ca93c0650
David StarBridge <ufocrew@gmail.com>
2025-10-09 22:52:20 +0800

    Update 00_close_other_qms.py
    with test connection

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/00_close_other_qms.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/00_close_other_qms.py
@@ -22,6 +22,14 @@ machine = QuAM.load()
 
 # Generate the OPX and Octave configurations
 config = machine.generate_config()
+
 # Open Communication with the QOP
-qmm = machine.connect()
+qmm = machine.connect() # Use this line if you want to connect to the QOP defined in the state file
+
+# from qm import QuantumMachinesManager
+# qmm = QuantumMachinesManager(host='10.21.19.201', cluster_name='QPX1000_2',port = 2001, log_level="DEBUG")
+# qmm = QuantumMachinesManager(host='10.31.19.131', cluster_name='QPX1000_2',port = 2002, log_level="DEBUG")
+# qmm = QuantumMachinesManager(host='192.168.1.27', cluster_name='QPX1000_2',port = 8002, log_level="DEBUG")
+# qmm = QuantumMachinesManager(host='192.168.1.225', cluster_name='QPX1000_2',port = 9510, log_level="DEBUG")
+
 qmm.close_all_qms()
