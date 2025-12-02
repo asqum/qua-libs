9252ed8b315cc43d0790cefc98f31af7d109b82e
Deepak Khurana <119570568+Deepakkhurrana@users.noreply.github.com>
2025-12-02 15:00:20 +0100

    Uncommenting the reset part

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/05b_T1_vs_coulper_flux.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/05b_T1_vs_coulper_flux.py
@@ -138,19 +138,19 @@ with program() as t1_vs_coupler_flux:
             qubit_pair.coupler.set_dc_offset(0.0)
         else:
             qubit_pair.coupler.to_decouple_idle()
-        # wait(1000)
+        wait(1000)
 
         with for_(n, 0, n < n_avg, n + 1):
             save(n, n_st)
             with for_(*from_array(flux_coupler, fluxes_coupler)):
                 with for_(*from_array(t, idle_times)):
-                    # if node.parameters.reset_type == "active":
-                    #     # active_reset(qubit, "readout")
-                    #     active_reset_simple(qubit, "readout")
-                    #     qubit_pair.align()
-                    # else:
-                    #     qubit.resonator.wait(qubit.thermalization_time * u.ns)
-                    #     qubit_pair.align()
+                    if node.parameters.reset_type == "active":
+                        # active_reset(qubit, "readout")
+                        active_reset_simple(qubit, "readout")
+                        qubit_pair.align()
+                    else:
+                        qubit.resonator.wait(qubit.thermalization_time * u.ns)
+                        qubit_pair.align()
                     
                     if "coupler_qubit_crosstalk" in qubit_pair.extras:
                         assign(comp_flux_qubit, arb_flux_bias_offset [qubit.name] + qubit_pair.extras["coupler_qubit_crosstalk"] * flux_coupler )
