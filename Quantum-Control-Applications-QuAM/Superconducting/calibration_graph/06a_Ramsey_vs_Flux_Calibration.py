e52770439bcad70a7f8ad86e68c3c984ea5b8645
HuanQM <huan.kung@quantum-machines.co>
2026-06-29 03:54:07 +0800

    remove 06a hard code

--- Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/06a_Ramsey_vs_Flux_Calibration.py
+++ Quantum-Control-Applications-QuAM/Superconducting/calibration_graph/06a_Ramsey_vs_Flux_Calibration.py
@@ -296,7 +296,7 @@ if not node.parameters.simulate:
                         qubit.z.joint_offset += flux_offset[qubit.name]
                         qubit.z.independent_offset = qubit.z.joint_offset - qubit.phi0_voltage / 2 
                 elif flux_point == "joint":
-                    qubit.z.joint_offset += flux_offset[qubit.name]/2
+                    qubit.z.joint_offset += flux_offset[qubit.name]
                 else:
                     raise RuntimeError(f"unknown flux_point")
                 qubit.freq_vs_flux_01_quad_term = float(a[qubit.name])
